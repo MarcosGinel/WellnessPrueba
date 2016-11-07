184d3289af2d61f29bf302d50d8558025874d974
Marcos Ginel <marcos.ginel@gmail.com>
2016-11-08 00:38:41 +0100

    * Arreglados más permisos de la API

--- facturacion/models.py
+++ facturacion/models.py
@@ -4,14 +4,17 @@ from django.db import models
 from django.db.models import Model
 
 class PrecioDiario(Model):
-    fecha = models.DateField(blank=False)
+    fecha = models.DateField(blank=False, unique=True)
     precio = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
 
     def __str__(self):
         return str(self.fecha)
 
 class ConsumoDiario(Model):
-    fecha = models.ForeignKey(PrecioDiario, unique=True)
+    fecha = models.ForeignKey(PrecioDiario)
     usuario = models.ForeignKey(User)
     cantidad = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together = (("fecha", "usuario"),)
 
